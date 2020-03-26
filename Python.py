ca60d3b684f71589c4e85f833c7f3d28d9fbac96
MKN2026 <mkn2026@gmail.com>
2020-03-26 12:06:07 -0700

    Added Python File

---
+++ Python.py
@@ -0,0 +1,16 @@
+# Welcome to my python file
+# This file will contain several different codes
+
+""" This command prints my name """
+print("Khizar Naeem")
+
+""" This command adds two numbers """
+my_age = 22
+your_age = 25
+our_age = (my_age+your_age)
+print(our_age)
+
+""" Learning list """
+grocery_list = "apple,banana,orange"
+
+print(grocery_list)
