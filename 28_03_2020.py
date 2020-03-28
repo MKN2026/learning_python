2fb6d7fa8cff6c6f8d99794f3d5f6c41d5aed482
MKN2026 <mkn2026@gmail.com>
2020-03-28 15:49:46 +0400

    Basic Math functions

---
+++ 28_03_2020.py
@@ -0,0 +1,23 @@
+# The date today is 28th of March 2020.
+
+# Number Set
+num_set = (28,54,79,37,2,9,0)
+print(num_set)
+
+# Lagest Number
+largest_num = (max(num_set))
+print("The largest number is", largest_num)
+
+# Smallest Number
+smallest_num = (min(num_set))
+print("The smallest number is", smallest_num)
+
+# Sum total of all numbers
+print("The total of these numbers is",sum(num_set))
+
+# Division of third and fourth digits
+# Seperating the digits
+div_num = (num_set[2], num_set[3])
+print("we are going to divide", div_num)
+ans = (divmod(num_set[2], num_set[3]))
+print("the answer for 79 divide by 37 is", ans)
