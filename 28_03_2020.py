99aeddc45ef94b1640ee9ad122575f7d5d25c070
MKN2026 <mkn2026@gmail.com>
2020-03-28 20:49:15 +0400

    Nothing New

--- 28_03_2020.py
+++ 28_03_2020.py
@@ -1,7 +1,8 @@
 # The date today is 28th of March 2020.
 
 # Number Set
-num_set = (28,54,79,37,2,9,0)
+global num_set
+num_set = [28,54,79,37,2,9,0]
 print(num_set)
 
 # Lagest Number
@@ -21,3 +22,13 @@ div_num = (num_set[2], num_set[3])
 print("we are going to divide", div_num)
 ans = (divmod(num_set[2], num_set[3]))
 print("the answer for 79 divide by 37 is", ans)
+
+""" Playing with RANDOM """
+
+# Creating a random number between 1 and 100
+import random
+
+print(random.randrange(1,100))
+
+
+
